012be652b3929c340d0fa8a175326449df4747f0
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2013-01-25 11:58:44 +0100

    Load the nib using UINib and keep it as an associated object to the class.

--- UIView+NibLoading.m
+++ UIView+NibLoading.m
@@ -6,15 +6,39 @@
 // 	https://github.com/n-b/UIView-NibLoading
 
 #import "UIView+NibLoading.h"
+#import "objc/objc-runtime.h"
 
 @implementation UIView(NibLoading)
+
++ (UINib*) _nibLoadingAssociatedNibWithName:(NSString*)nibName
+{
+    static char kUIViewNibLoading_associatedNibsKey;
+
+    NSDictionary * associatedNibs = objc_getAssociatedObject(self, &kUIViewNibLoading_associatedNibsKey);
+    UINib * nib = associatedNibs[nibName];
+    if(nil==nib)
+    {
+        nib = [UINib nibWithNibName:nibName bundle:nil];
+        if(nib)
+        {
+            NSMutableDictionary * newNibs = [NSMutableDictionary dictionaryWithDictionary:associatedNibs];
+            newNibs[nibName] = nib;
+            objc_setAssociatedObject(self, &kUIViewNibLoading_associatedNibsKey, [NSDictionary dictionaryWithDictionary:newNibs], OBJC_ASSOCIATION_RETAIN);
+        }
+    }
+
+    return nib;
+}
 
 - (void) loadContentsFromNibNamed:(NSString*)nibName
 {
     // Load the nib file, setting self as the owner.
     // The root view is only a container and is discarded after loading.
-    NSArray * views = [[NSBundle mainBundle] loadNibNamed:nibName owner:self options:nil];
-    NSAssert(views!=nil, @"UIView+NibLoading : Can't load nib named %@.",nibName);
+    UINib * nib = [[self class] _nibLoadingAssociatedNibWithName:nibName];
+    NSAssert(nib!=nil, @"UIView+NibLoading : Can't load nib named %@.",nibName);
+
+    NSArray * views = [nib instantiateWithOwner:self options:nil];
+    NSAssert(views!=nil, @"UIView+NibLoading : Can't instantiate nib named %@.",nibName);
     NSAssert(views.count==1, @"UIView+NibLoading : There must be exactly one root container view in %@.",nibName);
     UIView * containerView = [views objectAtIndex:0];
     NSAssert([[containerView class] isEqual:[UIView class]], @"UIView+NibLoading : The container view in nib %@ should be a UIView instead of %@. (It's only a container, and it's discarded after loading.)",nibName,[containerView class]);
